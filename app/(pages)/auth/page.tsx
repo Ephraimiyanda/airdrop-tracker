9008d719ad33a57678012617e1a50dfb0d46d5e0
Ephraimiyanda <iyandaephraim@gmail.com>
2024-12-20 11:48:37 +0100

    fix:dependencies installed

--- app/(pages)/auth/page.tsx
+++ app/(pages)/auth/page.tsx
@@ -96,7 +96,7 @@ export default function App() {
                   signIn("credentials", {
                     username: userName,
                     password,
-                    redirect: true,
+                    redirect: false,
                     callbackUrl: "/",
                   });
                 }}
